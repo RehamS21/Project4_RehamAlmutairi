28a5b06a0b009cd00d87bfe8a8f595c01f46d7bc
RehamS21 <reham.mu433@gmail.com>
2023-08-17 19:46:15 +0300

    initial commit

---
+++ HealthCareSystem_Project4/src/main/java/com/example/healthcaresystem_project4/HealthCareSystemProject4Application.java
@@ -0,0 +1,13 @@
+package com.example.healthcaresystem_project4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareSystemProject4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCareSystemProject4Application.class, args);
+    }
+
+}
---
+++ HealthCareSystem_Project4/src/test/java/com/example/healthcaresystem_project4/HealthCareSystemProject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthcaresystem_project4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareSystemProject4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
