c59287e280ad8c743fcf96e9d095a73ffb66cd61
RehamS21 <reham.mu433@gmail.com>
2023-08-17 20:24:57 +0300

    complete model

---
+++ HealthCareSystem_Project4/src/main/java/com/example/healthcaresystem_project4/Model/Doctor.java
@@ -0,0 +1,35 @@
+package com.example.healthcaresystem_project4.Model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Entity
+@RequiredArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "Doctor name must not null")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String name;
+    @NotEmpty(message = "position must not null")
+    @Column(columnDefinition = "varchar(20) not null, check(position = 'dentist' or position = 'general' or position = 'pediatrician')")
+    private String position;
+    @NotEmpty(message = "doctor phone number must not empty")
+    // saudi phone number. reference : https://gist.github.com/homaily/8672499
+    @Pattern(regexp = "^(009665|9665|\\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$")
+    @Column(columnDefinition = "varchar(10) unique not null")
+    private String phoneNo;
+    @NotNull(message = "doctor salary must not null")
+    @Positive(message = "doctor salary must be a positive")
+    @Column(columnDefinition = "int not null")
+    private Double salary;
+
+
+}
---
+++ HealthCareSystem_Project4/src/main/java/com/example/healthcaresystem_project4/Model/Patient.java
@@ -0,0 +1,39 @@
+package com.example.healthcaresystem_project4.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Entity
+@RequiredArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "patient name must not null")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String name;
+
+    @NotEmpty(message = "patient phone number must not null")
+    @Pattern(regexp = "^(009665|9665|\\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$")
+    @Column(columnDefinition = "varchar(10) unique not null")
+    private String phoneNo;
+
+    @NotNull(message = "age must not null")
+    @Positive(message = "age must be positive")
+    @Column(columnDefinition = "int(80) not null")
+    private Integer age;
+
+    @NotNull(message = "patient money must not null")
+    @Positive(message = "patient salary must be a positive")
+    @Column(columnDefinition = "int not null")
+    private Integer PatientMoney;
+}
